47ea51c0fed9f6a3f8ba28c0582ac66138299131
Renee Nordholm <reneenordholm@gmail.com>
2019-04-21 17:21:41 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -7,7 +7,7 @@ def countdown(number)
   "HAPPY NEW YEAR!"
 end
 
-def countdown_with_sleep
+def countdown_with_sleep(number)
   number = 10
     until number == 0
       puts "#{number} SECOND(S)!".sleep(5)
